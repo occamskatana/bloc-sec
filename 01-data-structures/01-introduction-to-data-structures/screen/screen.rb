20a5dc67614c983f8922c991d6f3e0bc7e343fd4
john carter <johngallweycarter@gmail.com>
2016-10-02 14:01:24 -0400

    assignment one completed as a note to Rob Madden, please see the changes to pixel_spec.rb - it just needed a reference to the file it was testing I added my own test to screen spec.rb to ensure the screen array was properly instantiated

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -1,4 +1,5 @@
 require_relative 'pixel'
+require 'matrix'
 
 class Screen
   attr_accessor :width
@@ -6,18 +7,24 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @width = width 
+    @height = height
+    @matrix = Matrix.build(height, width){nil}.to_a
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    @matrix[y][x] = pixel
   end
 
   def at(x, y)
+    @matrix[x][y]
   end
 
   private
 
   def inbounds(x, y)
+    
   end
 
 end
