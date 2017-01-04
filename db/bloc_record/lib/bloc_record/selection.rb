147be1f6f715f95904b6851b006be276a0d4104f
John Carter <johngallweycarter@gmail.com>
2017-01-03 23:34:26 -0500

    merging address_bloc and blocc_record over into general git repo

---
+++ db/bloc_record/lib/bloc_record/selection.rb
@@ -0,0 +1,22 @@
+require 'sqlite3'
+
+module Selection
+  def find(id)
+    row = connection.get_first_row <<-SQL
+	    SELECT #{columns.join ","} FROM #{table}
+	    WHERE id = #{id};
+    SQL
+
+    data = Hash[columns.zip(row)]
+    new(data)
+  end
+end
+
+
+def find_by(attribute, value)
+	results = []
+	connection.execute("SELECT * FROM #{table} WHERE #{attribute}=#{value};") do |row|
+		row << results
+	end
+	return results
+end
