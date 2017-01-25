5446e45d479baed0e9d567e3b7e42cb7ecda02f3
John Carter <johngallweycarter@gmail.com>
2017-01-25 12:19:13 -0500

    address_bloc changes committed

--- db/address_bloc/models/address_book.rb
+++ db/address_bloc/models/address_book.rb
@@ -3,14 +3,10 @@ require "csv"
 require 'bloc_record/base'
 
 class AddressBook < BlocRecord::Base
-  attr_reader :entries
+  has_many :entries
 
   def add_entry(name, phone_number, email)
    Entry.create(name: name, phone_number: phone_number, email: email, address_book_id: 1 )
-  end
-  
-  def entries
-    Entry.where(address_book_id: self.id)
   end
 
   def find_entry(name)
